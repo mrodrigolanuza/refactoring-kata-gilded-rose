7d68c239d8b59fc823d9a17c19bf0355889444d0
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2025-02-22 12:25:13 +0100

    Include snapshot testing and prepare for golden master

--- src/tests/gildedRose.test.ts
+++ src/tests/gildedRose.test.ts
@@ -2,10 +2,23 @@ import { Item, GildedRose } from '../core/gildedRose';
 
 describe('The Gilded Rose', () => {
 	it('updates quality for a new item', () => {
-		const gildedRose = new GildedRose([new Item('new item', 0, 0)]);
+		const items = generateCombinationOfItems(
+			['new item'], 
+			[0], 
+			[0]			
+		);
+		
+		const gildedRose = new GildedRose(items);
 
-		const items = gildedRose.updateQuality();
+		const updatedItems = gildedRose.updateQuality();
 
-		expect(items[0]).toEqual({"name": "new item", "quality": 0, "sellIn": -1});
+		expect(updatedItems[0]).toMatchSnapshot();
 	});
 });
+
+function generateCombinationOfItems(names:string[], sellInDays:number[], qualities:number[]) {
+	return names
+		.flatMap((name) => sellInDays
+			.flatMap( sellInDay => qualities
+				.flatMap(quality => new Item(name, sellInDays, quality))));
+}

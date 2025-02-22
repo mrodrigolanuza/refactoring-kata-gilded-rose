186484dde1a07a93db358c37fee70f26c1f69d21
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2025-02-22 13:39:37 +0100

    Increasing combination of items to test against snapshot

--- src/tests/gildedRose.test.ts
+++ src/tests/gildedRose.test.ts
@@ -2,23 +2,23 @@ import { Item, GildedRose } from '../core/gildedRose';
 
 describe('The Gilded Rose', () => {
 	it('updates quality for a new item', () => {
-		const items = generateCombinationOfItems(
-			['new item', 'Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros'], 
-			[-1, 0, 6, 11], 
-			[0, 1, 50]			
-		);
+		const items = generateCombinationOfItemsFrom(
+			['new item', 'Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros'],
+			[-1, 0, 6, 13],
+			[0, 1, 51]
+		)
 		
 		const gildedRose = new GildedRose(items);
 
 		const updatedItems = gildedRose.updateQuality();
 
-		expect(updatedItems[0]).toMatchSnapshot();
+		expect(updatedItems).toMatchSnapshot();
 	});
 });
 
-function generateCombinationOfItems(names:string[], sellInDays:number[], qualities:number[]) {
-	return names
-		.flatMap((name) => sellInDays
-			.flatMap( sellInDay => qualities
-				.flatMap(quality => new Item(name, sellInDay, quality))));
+function generateCombinationOfItemsFrom(names:string[], sellinDays: number[], qualities:number[]) {
+	return names.flatMap(
+		name => sellinDays.flatMap(
+			sellin => qualities.flatMap(
+				quality => new Item(name, sellin, quality))))
 }

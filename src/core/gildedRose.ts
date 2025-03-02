c4a1129cede03f2a05ec4c8d598a6830c20d4306
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2025-03-02 12:18:10 +0100

    Refactor: lift conditional Aged Brie

--- src/core/gildedRose.ts
+++ src/core/gildedRose.ts
@@ -21,34 +21,37 @@ export class GildedRose {
 	}
 
 	private updateQualityFor(item: Item){
-		if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-			if (item.quality > 0) {
-				if (item.name != 'Sulfuras, Hand of Ragnaros') {
-					item.quality = item.quality - 1
-				}
-			}
+		const isAgedBrie = item.name == 'Aged Brie';
+		if (isAgedBrie) {
+			this.updateAgedBrie(item);
 		} else {
-			if (item.quality < 50) {
-				item.quality = item.quality + 1
-				if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-					if (item.sellIn < 11) {
-						if (item.quality < 50) {
-							item.quality = item.quality + 1
-						}
+			if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+				if (item.quality > 0) {
+					if (item.name != 'Sulfuras, Hand of Ragnaros') {
+						item.quality = item.quality - 1
 					}
-					if (item.sellIn < 6) {
-						if (item.quality < 50) {
-							item.quality = item.quality + 1
+				}
+			} else {
+				if (item.quality < 50) {
+					item.quality = item.quality + 1
+					if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+						if (item.sellIn < 11) {
+							if (item.quality < 50) {
+								item.quality = item.quality + 1
+							}
+						}
+						if (item.sellIn < 6) {
+							if (item.quality < 50) {
+								item.quality = item.quality + 1
+							}
 						}
 					}
 				}
 			}
-		}
-		if (item.name != 'Sulfuras, Hand of Ragnaros') {
-			item.sellIn = item.sellIn - 1;
-		}
-		if (item.sellIn < 0) {
-			if (item.name != 'Aged Brie') {
+			if (item.name != 'Sulfuras, Hand of Ragnaros') {
+				item.sellIn = item.sellIn - 1;
+			}
+			if (item.sellIn < 0) {
 				if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
 					if (item.quality > 0) {
 						if (item.name != 'Sulfuras, Hand of Ragnaros') {
@@ -58,10 +61,18 @@ export class GildedRose {
 				} else {
 					item.quality = item.quality - item.quality
 				}
-			} else {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1
-				}
+			}
+		}
+	}
+
+	private updateAgedBrie(item: Item) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1
+		}
+		item.sellIn = item.sellIn - 1;
+		if (item.sellIn < 0) {
+			if (item.quality < 50) {
+				item.quality = item.quality + 1
 			}
 		}
 	}

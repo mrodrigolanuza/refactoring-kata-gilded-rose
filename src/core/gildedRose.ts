1724b53265b03c19a113790d786d4b346a28f14f
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2025-03-02 12:24:10 +0100

    Refactor: lift conditional BackStage Pass

--- src/core/gildedRose.ts
+++ src/core/gildedRose.ts
@@ -22,46 +22,49 @@ export class GildedRose {
 
 	private updateQualityFor(item: Item){
 		const isAgedBrie = item.name == 'Aged Brie';
+		const isBackStagePass = item.name == 'Backstage passes to a TAFKAL80ETC concert';
 		if (isAgedBrie) {
 			this.updateAgedBrie(item);
 		} else {
-			if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+			if (isBackStagePass) {
+				this.updateBackStagePass(item);
+			} else {
 				if (item.quality > 0) {
 					if (item.name != 'Sulfuras, Hand of Ragnaros') {
 						item.quality = item.quality - 1
 					}
 				}
-			} else {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1
-					if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-						if (item.sellIn < 11) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1
-							}
-						}
-						if (item.sellIn < 6) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1
-							}
-						}
-					}
+				if (item.name != 'Sulfuras, Hand of Ragnaros') {
+					item.sellIn = item.sellIn - 1;
 				}
-			}
-			if (item.name != 'Sulfuras, Hand of Ragnaros') {
-				item.sellIn = item.sellIn - 1;
-			}
-			if (item.sellIn < 0) {
-				if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+				if (item.sellIn < 0) {
 					if (item.quality > 0) {
 						if (item.name != 'Sulfuras, Hand of Ragnaros') {
 							item.quality = item.quality - 1
 						}
 					}
-				} else {
-					item.quality = item.quality - item.quality
 				}
 			}
+		}
+	}
+
+	private updateBackStagePass(item: Item) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1
+			if (item.sellIn < 11) {
+				if (item.quality < 50) {
+					item.quality = item.quality + 1
+				}
+			}
+			if (item.sellIn < 6) {
+				if (item.quality < 50) {
+					item.quality = item.quality + 1
+				}
+			}
+		}
+		item.sellIn = item.sellIn - 1;
+		if (item.sellIn < 0) {
+			item.quality = item.quality - item.quality
 		}
 	}
 
